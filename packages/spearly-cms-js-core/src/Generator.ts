35fdaa8289604570f1c55cda7e154b6d8181b85d
Mantaroh Yoshinaga <mantaroh@gmail.com>
2022-12-20 20:39:35 +0900

    Fix cms core bugs
    - Fix constructor parameter.
    - Skip generating field value when field is null.

--- packages/spearly-cms-js-core/src/Generator.ts
+++ packages/spearly-cms-js-core/src/Generator.ts
@@ -14,7 +14,7 @@ export class SpearlyJSGenerator {
     options: SpearlyJSGeneratorInternalOption
 
     constructor(apiKey: string, domain: string, options: SpearlyJSGeneratorOption | undefined = undefined) {
-        this.client = new SpearlyApiClient(apiKey, domain)
+        this.client = new SpearlyApiClient(domain, apiKey)
         this.options = {
             linkBaseUrl: options?.linkBaseUrl || "",
             dateFormatter:  options?.dateFormatter || function japaneseDateFormatter(date: Date) {
