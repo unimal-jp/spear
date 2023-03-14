29ec83c9b0344eae85f310f06ce15473b74e98d7
Mantaroh YOSHINAGA <mantaroh@gmail.com>
2023-03-14 14:11:52 +0900

    WIP: Inbrowser skelton implementation

---
+++ packages/spear-cli/src/browser/inMemoryMagic.ts
@@ -0,0 +1,22 @@
+
+
+/**
+ * inMemoryMagic
+ * This function will generate the static site from specified source.
+ * This function should be receive the specified source code structure and build option parameter like spear.config.mjs.
+ * 
+ */
+export default function inMemoryMagic() {
+    console.log("helle")
+
+    // Alternative for parsing component and page.
+    // We should prepare the component and page structure here.
+
+    // Make instance of cms-js-core generator.
+
+    // Inserting Component into component here.
+
+    // Inserting CMS Content 
+
+    // Dump the result of generated code.
+}
